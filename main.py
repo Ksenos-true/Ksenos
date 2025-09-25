572ea721310e41d2144ad4615da0477f057f0252
Ksenos-true <polezaevaksenia547@gmail.com>
2025-09-25 13:08:50 +0300

    hello

---
+++ main.py
@@ -0,0 +1,11 @@
+while True:
+    a = input("username@hostname:~$")
+    b = a.split()
+    if a == 'exit':
+        break
+    if len(b) == 0:
+        continue
+    if b[0] == "ls":
+        print(a)
+    if b[0] == "cd":
+        print(a)    
