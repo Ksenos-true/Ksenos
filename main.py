18038825bcf04a9a0bebd8c96bef63e9c9d18e7c
Ksenos-true <polezaevaksenia547@gmail.com>
2025-10-09 13:37:43 +0300

    Сообщение к коммиту

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import os
+
 while True:
-    a = input("username@hostname:~$")
+    username = os.getenv('USER', 'user')
+    hostname = os.uname().nodename if hasattr(os, 'uname') else 'localhost'
+    a = input(f"{username}@{hostname}:~$ ")
     b = a.split()
     if a == 'exit':
         break
     if len(b) == 0:
         continue
-    if b[0] == "ls":
+    if b[0] == "ls" or b[0] == "cd":
         print(a)
-    if b[0] == "cd":
-        print(a)    
